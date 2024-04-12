7f9404ebf69c54eca1b04d451a42578e377f0803
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-04-12 12:46:00 -0400

    Add Global Error Handlers

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express()
 
 // Routes ==> All the api for the endPoints
 
 app.get('/', (req, res, next) => {
+
+
     res.json({
         message: "Welcome to the Ebook API"
     })
 } )
 
+// Global Error Handlers
+app.use(globalErrorHandler)
 
 export default app;
